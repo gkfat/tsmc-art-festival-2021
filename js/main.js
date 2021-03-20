2044bb24c9f75462db6c68662c6cab5486a35f87
gkfat <gkgkdesign@gmail.com>
2021-03-21 00:13:49 +0800

    Basic layout

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
+$.getJSON("../assets/content.json", function(json) {
+  console.log(json)
+});
 
-$('h1').html('12345');
+/*
+** Functions-------------------------
+*/
+// Header 超過 KV 才出現的動畫
+let prevScrollpos = window.pageYOffset;
+window.onscroll = () => {
+  let currentScrollPos = window.pageYOffset;
+  if ( currentScrollPos > 500 ) { // 超過 KV
+    $('.section-nav').get(0).style.top = '0';
+  } else {
+    $('.section-nav').get(0).style.top = '-120px';
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
+
+$(document).ready(function() {
+  $('.section-nav').get(0).style.top = '-120px';
+});
