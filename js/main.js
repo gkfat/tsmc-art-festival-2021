8b45994190d28a0a82a3958e52a809f6a6498e76
gkfat <gkgkdesign@gmail.com>
2021-03-27 20:18:08 +0800

    Popup layout detail, adjust all font style & animations

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-$.getJSON("../assets/content.json", function(json) {
-  console.log(json)
-});
-
 /*
 ** Functions-------------------------
 */
@@ -10,9 +6,13 @@ let prevScrollpos = window.pageYOffset;
 window.onscroll = () => {
   let currentScrollPos = window.pageYOffset;
   if ( currentScrollPos > 200 ) { // 超過 KV
-    $('.section-nav').get(0).style.top = '0';
+    if ( currentScrollPos > $('#preface').get(0).offsetTop && currentScrollPos > prevScrollpos ) { // 往下滑隱藏
+      $('.section-nav').css('top', '-120px');
+    } else { // 往上滑
+      $('.section-nav').css('top', '0');
+    }
   } else {
-    $('.section-nav').get(0).style.top = '-120px';
+    $('.section-nav').css('top', '-120px');
   }
   prevScrollpos = currentScrollPos;
 }
@@ -25,14 +25,12 @@ $('.nav-items__li').on('click', function() {
   if ( !$(this).hasClass('active') ) {
     $(this).addClass('active');
   }
-  // checkDropdown();
 });
 $('.dropdown-btn__a').on('click', function() {
   $('.nav-items__li').removeClass('active');
   if ( !$(this).hasClass('active') ) {
     $(this).addClass('active');
   }
-  // checkDropdown();
 })
 
 // Dropdown Nav click function
@@ -43,25 +41,7 @@ $('.dropdown-item').on('click', function() {
   if ( !$(this).hasClass('active') ) {
     $(this).addClass('active');
   }
-  // checkDropdown();
 });
-
-// function checkDropdown() {
-//   if ( $('.dropdown').find('.active').length > 0 ) {
-//     $('.dropdown').css('display', 'block');
-//     $('.dropdown-btn .triangle').css({
-//       'display': 'block',
-//       'border-color': 'var(--darkGray) transparent transparent transparent'
-//     });
-//   } else {
-//     $('.dropdown').css('display', 'none');
-//     $('.dropdown-btn .triangle').css({
-//       'display': 'none',
-//       'border-color': 'var(--gray) transparent transparent transparent'
-//     });
-//   }
-// }
-
 
 // Events NAV click function
 $('.events-nav__li').on('click', function() {
@@ -110,7 +90,7 @@ function onYouTubeIframeAPIReady() {
   player = new YT.Player('yt-video', {
     height: '100%',
     width: '100%',
-    videoId: 'mFaJUtAnQDY',
+    videoId: 'WsRiDCf-UjU',
     events: {
       'onReady': onPlayerReady,
       'onStateChange': onPlayerStateChange
@@ -141,22 +121,65 @@ $('.section-video .btn').on('click', function() {
   player.playVideo();
 })
 
+let scrollY;
+let showingPopupId;
 // Popup toggle function
 $('.showPopup').on('click', function() {
-  $('.popup').show();
-  // $('.popup').css('transform', 'translateY(0)');
-  $('body').addClass('stop-scrolling')
+  scrollY = window.scrollY;
+  const classArray = $(this).attr('class').split(' ');
+  showingPopupId = classArray[classArray.length - 1];
+  $('.popup').show().scrollTop();
+  $(`#${showingPopupId}`).show();
+  $(`.${showingPopupId}_dots`).show();
+  $('body').addClass('stop-scrolling');
+  // 若點到 popup 外就關閉
+  $('.popup').on('click', function(e) {
+    if ( $('.popup-frame').has(e.target).length === 0 ) {
+      $('.popup').hide();
+      $(`#${showingPopupId}`).hide();
+      $(`.${showingPopupId}_dots`).hide();
+      $('body').removeClass('stop-scrolling')
+      $('html').css('scroll-behavior', 'auto');
+      window.scrollTo(0, scrollY);
+      $('html').css('scroll-behavior', 'smooth');
+    }
+  })
 })
 $('.closePopup').on('click', function() {
   $('.popup').hide();
-  // $('.popup').css('transform', 'translateY(100vh)');
+  $(`#${showingPopupId}`).hide();
+  $(`.${showingPopupId}_dots`).hide();
   $('body').removeClass('stop-scrolling')
+  $('html').css('scroll-behavior', 'auto');
+  window.scrollTo(0, scrollY);
+  $('html').css('scroll-behavior', 'smooth');
 })
 
 
 // 初始化
 $(document).ready(() => {
+  // Popup
   $('.popup').hide();
+  $('#popup_0').hide();
+  $('#popup_1').hide();
+  $('#popup_2').hide();
+  $('#popup_3').hide();
+  $('#popup_4').hide();
+  $('#popup_5').hide();
+  $('#popup_6').hide();
+  $('#popup_7').hide();
+  $('#popup_8').hide();
+  $('#popup_9').hide();
+  $('.popup_0_dots').hide();
+  $('.popup_1_dots').hide();
+  $('.popup_2_dots').hide();
+  $('.popup_3_dots').hide();
+  $('.popup_4_dots').hide();
+  $('.popup_5_dots').hide();
+  $('.popup_6_dots').hide();
+  $('.popup_7_dots').hide();
+  $('.popup_8_dots').hide();
+  $('.popup_9_dots').hide();
   // Header
   $('.section-nav').get(0).style.top = '-120px';
   // 節目場次 Slider
